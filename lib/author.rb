9031adba3f1c83f03dc7d5a594ad4691bec62fac
Joshua Needham <joshuabneedham@gmail.com>
2017-11-12 05:46:14 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,2 +1,28 @@
 class Author
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
+    post.artist = self
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post_by_name(name)
+    post = Post.new(name)
+    @posts << post
+    post.artist = self
+  end
+
+  def self.post_count
+    @posts.count
+  end
+
 end
